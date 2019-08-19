e6e6851461fb085fa8a2a8f950db0900e0e68fa6
southl <gyrii84@gmail.com>
2019-08-19 11:19:06 +0300

    add lesson 17 1st hw

--- lesson17/filedemo_string2.rb
+++ lesson17/filedemo_string2.rb
@@ -1,13 +1,19 @@
 input = File.open("task1.txt", "r")
 
-total = 0
+total = 0 
+x = 0
 
 while (line = input.gets)
 	arr = line.split ","
 	val = arr[1].to_i
 	total += val
 	
+	if x == 5
+	  break
+	end
+	x += 1
+	
 end
-
+puts x
 puts total
 input.close
---
+++ lesson17/matchIndex.rb
@@ -0,0 +1,9 @@
+list = [1,2,3,4,5,76]
+
+list.each_index do |i|
+	if i == 3
+
+		break
+	end
+	puts i
+end
